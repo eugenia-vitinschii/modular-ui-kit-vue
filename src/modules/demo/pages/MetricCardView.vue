5b2852ad8c187710d7dd51a2ea6ffd5f6cc527da
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2026-09-03 17:14:20 +0300

    feat(muk-metric-card): create demo view for component

---
+++ src/modules/demo/pages/MetricCardView.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="muk-page">
+      <div class="muk-container">
+         <div class="muk-page__wrapper">
+            <div class="muk-page__header">
+               <muk-text as="h1" :type="'muk-heading'">Metric Cards Preview</muk-text>
+            </div>
+            <div class="muk-page__content">
+               <div class="muk-section">
+                  <muk-text> Demo controls</muk-text>
+               </div>
+               <div class="muk-section">
+                  <muk-metric-card></muk-metric-card>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+
+<script setup lang="ts">
+/* Components */
+import MukText from '@/components/ui/typography/MukText.vue';
+import MukMetricCard from '@/components/ui/data-display/MukMetricCard.vue';
+
+
+</script>
